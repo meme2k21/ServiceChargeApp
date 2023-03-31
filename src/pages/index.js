82176148d3fae36659754320b2808e0d1a171fc4
meme2k21 <alyssajumapao17@gmail.com>
2023-03-31 08:36:45 +0800

    some frontend

--- src/pages/index.js
+++ src/pages/index.js
@@ -117,8 +117,8 @@ function dashboard() {
   const sortedData = tickets?.sort(
     (a, b) => new Date(b.date_created) - new Date(a.date_created)
   );
-
-  console.log(sortedData.length);
+    console.log(sortedData);
+  // const agingTicket = sortedData.filter(ticket => {ticket.date_created});
 
   return (
     <div style={{ background: "white" }}>
@@ -137,13 +137,37 @@ function dashboard() {
           New Ticket
         </button>
         <br/><br/>
-        {/* <div style={{ background: "#F27B53", padding: "20px", borderRadius: "5px", display: "flex", alignItems: "center", justifyContent: "center", maxWidth:'200px' }}>
-          <div style={{ fontSize: "24px", fontWeight: "bold", marginRight: "10px"}}> {sortedData.length} </div>
-          <div style={{ fontSize: "16px" }}>ticket/s</div>
-        </div> */}
 
         <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", gridGap: "20px", color: "white" }}>
           <div style={{ background: "#F27B53", padding: "20px", borderRadius: "5px", display:'flex', alignItems:'center', position: 'relative' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={50} fill="none">
+              <path fill="white" fill-rule="evenodd" d="M3 5a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v16a1 1 0 0 1-1.625.78l-1.929-1.542-2.391 1.594a1 1 0 0 1-1.18-.051L12 20.28l-1.875 1.5a1 1 0 0 1-1.18.051l-2.391-1.594-1.93 1.543A1 1 0 0 1 3 21V5zm5 1a1 1 0 0 0 0 2h8a1 1 0 1 0 0-2H8zm0 4a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2H8zm0 4a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2H8z" clip-rule="evenodd"></path>
+              </svg>
+            <div style={{ fontSize: "24px", fontWeight: "bold", marginRight: "10px"}}> {sortedData.length} </div>
+            <div style={{ fontSize: "16px" }}>{(sortedData.length>1) ? 'tickets':'ticket'}</div>
+            <a onClick={() => {
+            router.push("/ticket/create");
+            }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16" style={{ position: 'absolute', bottom: '10px', right: '10px' }}> 
+                <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z" fill="white"></path> <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" fill="white"></path> <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" fill="white"></path> 
+              </svg>
+            </a>
+          </div>
+          <div style={{ background: "#DF577B", padding: "20px", borderRadius: "5px", display:'flex', alignItems:'center', position: 'relative' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={50} fill="none">
+              <path fill="white" fill-rule="evenodd" d="M3 5a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v16a1 1 0 0 1-1.625.78l-1.929-1.542-2.391 1.594a1 1 0 0 1-1.18-.051L12 20.28l-1.875 1.5a1 1 0 0 1-1.18.051l-2.391-1.594-1.93 1.543A1 1 0 0 1 3 21V5zm5 1a1 1 0 0 0 0 2h8a1 1 0 1 0 0-2H8zm0 4a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2H8zm0 4a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2H8z" clip-rule="evenodd"></path>
+            </svg>
+            <div style={{ fontSize: "24px", fontWeight: "bold", marginRight: "10px"}}> {sortedData.length} </div>
+            <div style={{ fontSize: "16px" }}>{(sortedData.length>1) ? 'tickets':'ticket'}</div>
+            <a onClick={() => {
+              router.push("/ticket/create");
+            }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16" style={{ position: 'absolute', bottom: '10px', right: '10px' }}> 
+                <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z" fill="white"></path> <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" fill="white"></path> <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" fill="white"></path> 
+              </svg>
+            </a>
+          </div>
+          <div style={{ background: "#847CC5", padding: "20px", borderRadius: "5px", display:'flex', alignItems:'center', position: 'relative' }}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={50} fill="none">
               <path fill="white" fill-rule="evenodd" d="M3 5a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v16a1 1 0 0 1-1.625.78l-1.929-1.542-2.391 1.594a1 1 0 0 1-1.18-.051L12 20.28l-1.875 1.5a1 1 0 0 1-1.18.051l-2.391-1.594-1.93 1.543A1 1 0 0 1 3 21V5zm5 1a1 1 0 0 0 0 2h8a1 1 0 1 0 0-2H8zm0 4a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2H8zm0 4a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2H8z" clip-rule="evenodd"></path>
               </svg>
@@ -157,9 +181,20 @@ function dashboard() {
               </svg>
             </a>
           </div>
-          <div style={{ background: "#f2f2f2", padding: "20px", borderRadius: "5px" }}>Box 2</div>
-          <div style={{ background: "#f2f2f2", padding: "20px", borderRadius: "5px" }}>Box 3</div>
-          <div style={{ background: "#f2f2f2", padding: "20px", borderRadius: "5px" }}>Box 4</div>
+          <div style={{ background: "#28B79B", padding: "20px", borderRadius: "5px", display:'flex', alignItems:'center', position: 'relative' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={50} fill="none">
+              <path fill="white" fill-rule="evenodd" d="M3 5a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v16a1 1 0 0 1-1.625.78l-1.929-1.542-2.391 1.594a1 1 0 0 1-1.18-.051L12 20.28l-1.875 1.5a1 1 0 0 1-1.18.051l-2.391-1.594-1.93 1.543A1 1 0 0 1 3 21V5zm5 1a1 1 0 0 0 0 2h8a1 1 0 1 0 0-2H8zm0 4a1 1 0 1 0 0 2h8a1 1 0 1 0 0-2H8zm0 4a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2H8z" clip-rule="evenodd"></path>
+            </svg>
+            <div style={{ fontSize: "24px", fontWeight: "bold", marginRight: "10px"}}> {sortedData.length} </div>
+            <div style={{ fontSize: "16px" }}>{(sortedData.length>1) ? 'tickets':'ticket'}</div>
+            <a onClick={() => {
+              router.push("/ticket/create");
+            }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16" style={{ position: 'absolute', bottom: '10px', right: '10px' }}> 
+                <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z" fill="white"></path> <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" fill="white"></path> <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" fill="white"></path> 
+              </svg>
+            </a>
+          </div>
         </div>
 
 
--- src/pages/tickets/index.js
+++ src/pages/tickets/index.js
@@ -1,5 +1,12 @@
+import axios from "axios";
 import Head from "next/head";
-import React, {useState} from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -12,17 +19,56 @@ export const getStaticProps = async () => {
 
 // for ticketManagement
 
-function Tickets({ tickets }) {
+function Tickets() {
+  const router = useRouter();
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/tickets")
+      .then((response) => {
+        setTickets(response?.data);
+      })
+      .catch((err) => console.log(err));
+  }, [selectedRows]);
+
+  const handleButtonClick = (row) => {
+    console.log("clicked Ticket #" + row.ticket_id);
+  };
+
+  const handleChange = useCallback((state) => {
+    setSelectedRows(state?.selectedRows);
+  }, []);
+
+  const handleDeleteRow = (row) => {
+    axios
+      .delete("http://localhost:8080/ticket/delete/" + row.ticket_id)
+      .then((response) => {
+        console.log("Ticket deleted:", response?.data);
+        // Update tickets state by filtering out the deleted ticket
+        setTickets((prevTickets) =>
+          prevTickets?.filter((ticket) => ticket?.ticket_id !== row?.ticket_id)
+        );
+      })
+      .catch((error) => {
+        console.error("Error deleting ticket:", error);
+      });
+  };
+
   const [ticketHealth, setTicketHealth] = useState('Ticket Health');
   const [ticketStatus, setTicketStatus] = useState('Ticket Status');
   const [ticketYearCreated, setTicketYearCreated] = useState('Year Created');
 
   //Modal Delete component
   const [showModal, setShowModal] = useState(false);
+  const [clickedId, setClickedId] = useState('');
 
-  const handleShowModal = () => {
+  const handleShowModal = (id) => {
       setShowModal(true);
       console.log(showModal);
+      setClickedId(id);
   };
 
   const handleDelete = () => {
@@ -34,28 +80,32 @@ function Tickets({ tickets }) {
       setShowModal(false);
   };
 
+
+  console.log(tickets);
   return (
     <>
       <Head>
         <title>Tickets</title>
         <meta name="keywords" content="tickets" />
       </Head>
+
+      {/* For Ticket Search Filtering */}
       <div  style={{ backgroundColor: '#D9D9D9', paddingLeft:'10px', fontWeight:'bold', width: '100vw-10px', height: '30px', display: 'flex', alignItems: 'center' }}> Search Filter</div>
       <div  style={{ backgroundColor: 'white', border:'5px solid white', borderColor:'#D9D9D9', paddingLeft:'10px', fontWeight:'bold', width: '100vw-10px', height: '60px', display: 'flex', alignItems: 'center', justifyContent:'space-between' }}> 
         <div style={{display:'flex', alignItems:'center'}}>
-          <select value={ticketHealth} style={{opacity: ticketHealth ==="Ticket Health"?0.5:1, border:'1px solid black', marginRight:'20px', height:'40px', width:'227px', fontStyle:'italic'}} >
+          <select value={ticketHealth} style={{opacity: ticketHealth ==="Ticket Health"? 0.5 : 1, border:'1px solid black', marginRight:'20px', height:'40px', width:'227px', fontStyle:'italic'}} >
             <option disabled>{ticketHealth}</option>
             <option>1</option>
             <option>2</option>
             <option>3</option>
           </select>
-          <select value={ticketStatus} style={{border:'1px solid black', marginRight:'20px', height:'40px', width:'227px', fontStyle:'italic'}} >
+          <select value={ticketStatus} style={{opacity: ticketStatus ==="Ticket Status"? 0.5 : 1, border:'1px solid black', marginRight:'20px', height:'40px', width:'227px', fontStyle:'italic'}} >
             <option disabled>{ticketStatus}</option>
             <option>1</option>
             <option>2</option>
             <option>3</option>
           </select>
-          <select value={ticketYearCreated} style={{border:'1px solid black', marginRight:'20px', height:'40px', width:'227px', fontStyle:'italic'}} >
+          <select value={ticketYearCreated} style={{opacity: ticketYearCreated ==="Year Created"? 0.5 : 1, border:'1px solid black', marginRight:'20px', height:'40px', width:'227px', fontStyle:'italic'}} >
             <option disabled>{ticketYearCreated}</option>
             <option>1</option>
             <option>2</option>
@@ -67,107 +117,78 @@ function Tickets({ tickets }) {
           <button style={{backgroundColor:'#D9D9D9', color:'black', fontSize:'16px', marginRight:'10px', height:'40px', width:'227px', fontStyle:'italic'}} >Clear</button>
         </div>
       </div>
-      <div style={{marginTop:'10px', marginBottom: '-40px'}}>
-        <input type='search' placeholder='Search' style={{border: '1px solid black', color:'black', height:'40px', width:'350px'}}></input>
+
+      {/* For ticket searching */}
+      <div style={{marginTop:'50px', marginBottom: '-40px'}}>
+        {/* <input type='search' placeholder='Search' style={{border: '1px solid black', color:'black', height:'40px', width:'350px'}}></input>
         <button type='search' style={{backgroundColor:'#D9D9D9', fontSize:'16px', marginRight:'15px', color:'black', height:'40px', width:'227px', float:'right'}}>Add Email Reminder</button>
         <select type='search' style={{backgroundColor:'#D9D9D9', fontSize:'16px', marginRight:'10px', color:'black', height:'40px', width:'227px', float:'right'}}>
           <option>Ticket Number</option>
           <option>Ticket Subject</option>
           <option>Ticket Status</option>
-        </select>
+        </select> */}
+        <div style={{ background: "#F27B53", padding: "20px", borderRadius: "5px", display:'flex', alignItems:'center', position: 'relative', width: '15vw' }}>
+          <a onClick={() => {
+            router.push("/ticket/create");
+            }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16"> 
+              <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z" fill="white"></path> <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" fill="white"></path> <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" fill="white"></path> 
+            </svg>
+            <label style={{color:'white'}}>New Ticket</label>
+          </a>
+        </div>
+        
       </div>
       <br /><br />
-      <table style={{borderBottom:'2px solid black', marginBottom:'10px'}}>
+      
+      {/* table display */}
+      <table style={{borderBottom:'2px solid black', marginBottom:'10px', paddingLeft:'1000px', textAlign:'left'}}>
         <thead>
-          <tr style={{backgroundColor:'#D9D9D9', color:'black', fontSize:'16px'}}>
-            <td>Ticket Number</td>
-            <td>Ticket Subject</td>
-            <td>Ticket Description</td>
-            <td>Date Created</td>
-            <td>Status</td>
+          <tr style={{backgroundColor:'#28B79B', color:'black', fontSize:'16px'}}>
+            <td className="table-cell flex-row" >Ticket Number</td>
+            <td className="table-cell">Ticket Subject</td>
+            <td className="table-cell">Ticket Description</td>
+            <td className="table-cell">Date Created</td>
+            <td className="table-cell">Status</td>
+            <td className="table-cell">Action</td>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td onClick={handleShowModal}>
-              <a href="" onClick={(e)=> e.preventDefault()} title='update'>Case Filed</a>
-            </td>
-              {showModal && <Modal onYes={handleDelete} onNo={handleCancel} />}
-          </tr>
-          <tr>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td onClick={handleShowModal}>
-              <a href="" onClick={(e)=> e.preventDefault()} title='update'>Case Filed</a>
-            </td>
-              {showModal && <Modal onYes={handleDelete} onNo={handleCancel} />}
-          </tr>
-          <tr>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td onClick={handleShowModal}>
-              <a href="" onClick={(e)=> e.preventDefault()} title='update'>Case Filed</a>
-            </td>
-              {showModal && <Modal onYes={handleDelete} onNo={handleCancel} />}
-          </tr>
-          <tr>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td onClick={handleShowModal}>
-              <a href="" onClick={(e)=> e.preventDefault()} title='update'>Case Filed</a>
-            </td>
-              {showModal && <Modal onYes={handleDelete} onNo={handleCancel} />}
-          </tr>
-          <tr>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td onClick={handleShowModal}>
-              <a href="" onClick={(e)=> e.preventDefault()} title='update'>Case Filed</a>
-            </td>
-              {showModal && <Modal onYes={handleDelete} onNo={handleCancel} />}
-          </tr>
-          <tr>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td onClick={handleShowModal}>
-              <a href="" onClick={(e)=> e.preventDefault()} title='update'>Case Filed</a>
-            </td>
-              {showModal && <Modal onYes={handleDelete} onNo={handleCancel} />}
-          </tr>
-          <tr>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td onClick={handleShowModal}>
-              <a href="" onClick={(e)=> e.preventDefault()} title='update'>Case Filed</a>
-            </td>
-              {showModal && <Modal onYes={handleDelete} onNo={handleCancel} />}
-          </tr>
-          <tr>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td>sample</td>
-            <td onClick={handleShowModal}>
-              <a href="" onClick={(e)=> e.preventDefault()} title='update'>Case Filed</a>
-            </td>
-              {showModal && <Modal onYes={handleDelete} onNo={handleCancel} />}
-          </tr>
+          {tickets.map(ticket => {
+            console.log('mapping');
+            return (
+              <tr key={ticket.ticket_id} className='table-row' style={{borderBottom: '1px solid rgba(0, 0, 0, 0.3)'}}>
+                <td className="table-cell flex-row" >
+                  {ticket.ticket_id}
+                </td>
+                <td className="table-cell">
+                  {ticket.ticket_title}
+                </td>
+                <td className="table-cell">
+                  {ticket.ticket_description}
+                </td>
+                <td className="table-cell">
+                  {ticket.date_created}
+                </td>
+                <td className="table-cell" onClick={() => handleShowModal(ticket.ticket_id)}>
+                  <a href="" onClick={(e)=> e.preventDefault()} title='update'>
+                    {ticket.ticket_status}
+                  </a>
+                </td>
+              {showModal && clickedId===ticket.ticket_id && <ModalComponent title='DeleteModal' label={`Are you sure to delete ${ticket.ticket_title}?`} onYes={handleDelete} onCancel={handleCancel} show={showModal}/>}
+              <td onClick={() => handleShowModal(ticket.ticket_id)}>
+                <a href="" onClick={(e)=> e.preventDefault()} title="delete">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='30' height='30'> 
+                    <g> 
+                      <path fill="none" d="M0 0h24v24H0z"/> 
+                      <path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z"/> 
+                    </g> 
+                  </svg>
+                </a>
+              </td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent:'space-between', marginBottom:'-50px' }}>
@@ -184,37 +205,50 @@ function Tickets({ tickets }) {
 export default Tickets;
 
 
+export const ModalComponent = ({ title, label, onYes, onCancel, show }) => {
 
-export function Modal( {onYes, onNo}) {
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    backgroundColor:'white', 
+    height:'25%'
+  };
 
   return (
-    <div 
-      style={{ 
-        position: 'fixed',
-        display: "flex", 
-        backgroundColor: "rgba(0, 0, 0, 0.1)", 
-        top: "0", 
-        left: "0", 
-        width: "100vw", 
-        height: "100%", 
-        zIndex: "1",
-        alignItems:'center',
-        justifyContent: 'center' }}>
-          <div 
-            className="modal-content position-absolute" 
-            style={{ 
-              backgroundColor: "#333333",
-              border: "1px solid #888", 
-              width: "80%", 
-              maxWidth: "600px", 
-              padding: "20px" }}>
-              <h6 style={{ color: 'white', textAlign: 'center' }}>Update ticket?</h6>
-              <br />
-              <div className="button-group" style={{ textAlign: 'right', marginTop: "-1em" }}>
-                  <button className="button danger" style={{ width: '7em', background:'#963634' }} onClick={onYes}>Yes</button>
-                  <button className="button" style={{ width: '7em', marginLeft: "2em", background:'#963634'  }} onClick={onNo}>No</button>
+          <Modal
+            open={show}
+            onClose={onCancel}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            className="modal d-block"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+          >
+            <div className="modal-dialog modal-dialog-centered" style={style}>
+              <div className="modal-header" style={{padding: '10px'}} >
+                <h5 className="modal-title">{title}</h5>
+                <svg
+                    style={{ position: 'absolute', top: '5px', right: '5px', cursor: 'pointer', border:'1px solid gray' }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"
+                    onClick={onCancel}
+                >
+                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" fill="blue"></path>
+                </svg>
               </div>
-          </div>
-    </div>
-);
-}
+              <div className="modal-body" style={{paddingLeft: '3%'}}>
+                <h6 style={{ color: 'black' }}>{label}</h6>
+              </div>
+              <div className="modal-footer" style={{ display: 'flex', justifyContent: 'space-between', position:'absolute', bottom: '10px', right: '10%', alignItems:'center', textAlign:'center'}}>
+                <button type="button" className="btn btn-primary" onClick={onYes} style={{ flex: '1 0 auto', backgroundColor:'blue', marginRight:'10%' }}>Yes</button>
+                <button type="button" className="btn btn-primary" onClick={onCancel} style={{ flex: '1 0 auto', backgroundColor: '#929292' }}>Cancel</button>
+              </div>
+            </div>
+          </Modal>
+  );
+};
