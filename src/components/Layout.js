94e380a5ce112899525a857da31f2cf6d38cad38
arson07 <adrian.roxas.son@gmail.com>
2023-04-27 06:55:26 +0800

    Added /forgotpassword in const hideNavbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import AdminNavbar from "./AdminNavbar";
 import ClientNavbar from "./ClientNavbar";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 function Layout({ children }) {
   const router = useRouter();
@@ -13,24 +13,41 @@ function Layout({ children }) {
   const [email, setEmail] = useState(null);
 
   useEffect(() => {
-    setId(localStorage.getItem('id'));
-    setUsername(localStorage.getItem('username'));
-    setRole(localStorage.getItem('role'));
-    setEmail(localStorage.getItem('email'));
+    setId(localStorage.getItem("id"));
+    setUsername(localStorage.getItem("username"));
+    setRole(localStorage.getItem("role"));
+    setEmail(localStorage.getItem("email"));
   }, []);
 
-  const hideNavbar = router.pathname === '/login' || router.pathname === '/register' || router.pathname === '/';
+  const hideNavbar =
+    router.pathname === "/login" ||
+    router.pathname === "/register" ||
+    router.pathname === "/" ||
+    router.pathname === "/forgotpassword";
   const isAdmin = `${role}` !== `1`;
 
   return (
-    <div className="content" 
-    style={!hideNavbar? 
-          {display:'flex', flexDirection:'column', minHeight:'100vh' }: 
-          {display:'flex', flexDirection:'column', minHeight:'100vh', backgroundImage:'url(bgAll.jpg)', backgroundSize:'cover', backgroundPosition:'center', boxShadow: 'inset 300px 300px 1000px rgba(0, 0, 0, .9)'}
-          }>
-      {!hideNavbar ? isAdmin? <AdminNavbar />:<ClientNavbar />: <Navbar />}
-      <div style={{paddingTop:'80px', margin:'20px', flexGrow:1}} >{children}</div>
-      <Footer/>
+    <div
+      className="content"
+      style={
+        !hideNavbar
+          ? { display: "flex", flexDirection: "column", minHeight: "100vh" }
+          : {
+              display: "flex",
+              flexDirection: "column",
+              minHeight: "100vh",
+              backgroundImage: "url(bgAll.jpg)",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              boxShadow: "inset 300px 300px 1000px rgba(0, 0, 0, .9)",
+            }
+      }
+    >
+      {!hideNavbar ? isAdmin ? <AdminNavbar /> : <ClientNavbar /> : <Navbar />}
+      <div style={{ paddingTop: "80px", margin: "20px", flexGrow: 1 }}>
+        {children}
+      </div>
+      <Footer />
     </div>
   );
 }
